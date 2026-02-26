e2afe6bb93dc909e3c0257c3b80e78241e48e9d3
Diego Duarte <diegocavalcante040307@gmail.com>
2026-02-26 18:10:16 -0300

    Documentação com Swagger e Adicionado GlobalExceptionHandler

---
+++ src/main/java/com/diegoduarte/library_api/controller/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.diegoduarte.library_api.controller;
+
+import com.diegoduarte.library_api.infrastructure.exceptions.ConflictException;
+import com.diegoduarte.library_api.infrastructure.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleConflictException(ConflictException ex) {
+        String mensagem = (ex.getMessage() != null) ? ex.getMessage() : "Erro interno na operação";
+        return new ResponseEntity<>(mensagem, HttpStatus.CONFLICT);
+    }
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        String mensagem = (ex.getMessage() != null) ? ex.getMessage() : "Erro interno na operação";
+        return new ResponseEntity<>(mensagem, HttpStatus.NOT_FOUND);
+    }
+
+
+}
